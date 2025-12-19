ec118b46fb317991693ad8c8af02660839f081f0
D1GQ <yourboylax@gmail.com>
2025-12-19 07:55:10 -0600

    add xml sum

--- src/Enums/PlayerTeam.cs
+++ src/Enums/PlayerTeam.cs
@@ -1,9 +1,27 @@
 ﻿namespace ReplantedOnline.Enums;
 
+/// <summary>
+/// Represents the team affiliation of a player in the game.
+/// </summary>
 internal enum PlayerTeam
 {
+    /// <summary>
+    /// Unassigned.
+    /// </summary>
     None,
+
+    /// <summary>
+    /// The Plants team.
+    /// </summary>
     Plants,
+
+    /// <summary>
+    /// The Zombies team.
+    /// </summary>
     Zombies,
+
+    /// <summary>
+    /// Spectators who are observing the game.
+    /// </summary>
     Spectators
 }
