a6f887090db0a13ec7baf414bde0cea3e8787e8f
D1GQ <yourboylax@gmail.com>
2025-12-20 15:39:07 -0600

    Play huge wave sound

---
+++ src/Patches/Gameplay/Versus/Zombies/FlagZombiePatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using Il2CppReloaded.Gameplay;
+using ReplantedOnline.Modules;
+using static Il2CppReloaded.Constants;
+using Zombie = Il2CppReloaded.Gameplay.Zombie;
+
+namespace ReplantedOnline.Patches.Gameplay.Versus.Zombies;
+
+[HarmonyPatch]
+internal static class FlagZombiePatch
+{
+    [HarmonyPatch(typeof(Zombie), nameof(Zombie.ZombieInitialize))]
+    [HarmonyPrefix]
+    private static void DropHead_Prefix(ZombieType theType)
+    {
+        if (theType is ZombieType.Flag)
+        {
+            Instances.GameplayActivity.m_audioService.PlaySample(Sound.SOUND_HUGE_WAVE);
+        }
+    }
+}
