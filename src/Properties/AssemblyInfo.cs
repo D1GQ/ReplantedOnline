a96dca1dbb33004a07bf6dd1ff5866b6bd8e65cf
D1GQ <yourboylax@gmail.com>
2025-12-19 22:23:27 -0600

    Fix version formatting

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: MelonInfo(typeof(ReplantedOnlineMod), ModInfo.MOD_NAME, $"{ModInfo.MOD_VERSION} / {ModInfo.MOD_RELEASE} / {ModInfo.RELEASE_DATE}", ModInfo.CREATOR, ModInfo.GITHUB)]
+[assembly: MelonInfo(typeof(ReplantedOnlineMod), ModInfo.MOD_NAME, $"{ModInfo.MOD_VERSION}-{ModInfo.MOD_RELEASE}-{ModInfo.RELEASE_DATE}", ModInfo.CREATOR, ModInfo.GITHUB)]
 [assembly: MelonGame(ModInfo.PVZR.COMPANY, ModInfo.PVZR.GAME)]
 [assembly: MelonAdditionalDependencies(ModInfo.BloomEngine.BLOOM_ENGINE_DEPENDENCY)]
 [assembly: HarmonyDontPatchAll]
