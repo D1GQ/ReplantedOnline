332b2fb7aca8f51c8c390daa07798df992306b05
D1GQ <yourboylax@gmail.com>
2026-02-27 11:14:51 -0600

    Fix build error

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -2,13 +2,11 @@ using MelonLoader;
 using ReplantedOnline;
 using System.Reflection;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: MelonInfo(typeof(ReplantedOnlineMod), ModInfo.MOD_NAME, ModInfo.MOD_VERSION_FORMATTED, ModInfo.CREATOR, ModInfo.GITHUB)]
-[assembly: MelonGame(ModInfo.PVZR.COMPANY, ModInfo.PVZR.GAME)]
-[assembly: MelonAdditionalDependencies(ModInfo.BloomEngine.BLOOM_ENGINE_DEPENDENCY)]
 [assembly: AssemblyTitle(nameof(ReplantedOnline))]
 [assembly: AssemblyProduct(nameof(ReplantedOnline))]
 [assembly: AssemblyVersion(ModInfo.MOD_VERSION)]
 [assembly: AssemblyFileVersion(ModInfo.MOD_VERSION)]
+
+[assembly: MelonInfo(typeof(ReplantedOnlineMod), ModInfo.MOD_NAME, ModInfo.MOD_VERSION_FORMATTED, ModInfo.CREATOR, ModInfo.GITHUB)]
+[assembly: MelonGame(ModInfo.PVZR.COMPANY, ModInfo.PVZR.GAME)]
+[assembly: MelonAdditionalDependencies(ModInfo.BloomEngine.BLOOM_ENGINE_DEPENDENCY)]
