4c286534315c2d6a333790f83bf115a132d2d5bf
D1GQ <yourboylax@gmail.com>
2026-03-02 00:13:26 -0600

    Bump beta

--- src/ModInfo.cs
+++ src/ModInfo.cs
@@ -32,7 +32,7 @@ internal static class ModInfo
     /// <summary>
     /// The number of the release.
     /// </summary>
-    internal const string MOD_RELEASE_INFO = "5";
+    internal const string MOD_RELEASE_INFO = "6";
 
     /// <summary>
     /// The formatted version string of the mod using semantic versioning.
