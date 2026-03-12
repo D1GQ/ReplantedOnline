510cfb8fd1ed364ef84a565a7eed38f60c406ba7
D1GQ <yourboylax@gmail.com>
2026-03-12 13:19:35 -0500

    Fix Melonpult splash damage effecting more than one gravestone or target zombie at a time

---
+++ src/Patches/Gameplay/Versus/ProjectilePatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Il2CppReloaded.Gameplay;
+using ReplantedOnline.Network.Client;
+using ReplantedOnline.Utilities;
+
+namespace ReplantedOnline.Patches.Gameplay.Versus;
+
+[HarmonyPatch]
+internal static class ProjectilePatch
+{
+    [HarmonyPatch(typeof(Projectile), nameof(Projectile.IsZombieHitBySplash))]
+    [HarmonyPostfix]
+    private static void Projectile_IsZombieHitBySplash_Postfix(Projectile __instance, Zombie theZombie, ref bool __result)
+    {
+        if (__instance.mProjectileType != ProjectileType.Melon) return;
+
+        if (NetLobby.AmInLobby())
+        {
+            // Prevent target zombies and gravestones from taking Melonpult splash damage
+            // Don't have to worry about target zombies and gravestones taking damage from behind due to ZombiePatch.Zombie_GetZombieRect_Postfix
+            if (theZombie.mZombieType.IsGravestoneOrTarget())
+            {
+                if (__instance.mRow != theZombie.mRow)
+                {
+                    __result = false;
+                }
+            }
+        }
+    }
+}
