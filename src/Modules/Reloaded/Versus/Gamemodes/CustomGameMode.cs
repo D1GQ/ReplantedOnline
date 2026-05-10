b47c95a5ebbe9ddc66f51ca772e0863cee58e21c
D1GQ <yourboylax@gmail.com>
2026-05-10 17:11:16 -0500

    Fix custom seed selection navigation for Gamepad

--- src/Modules/Reloaded/Versus/Gamemodes/CustomGameMode.cs
+++ src/Modules/Reloaded/Versus/Gamemodes/CustomGameMode.cs
@@ -2,6 +2,7 @@
 
 using Il2Cpp;
 using Il2CppReloaded.Gameplay;
+using Il2CppReloaded.Input;
 using Il2CppSource.Binders;
 using ReplantedOnline.Attributes.Register;
 using ReplantedOnline.Enums.Versus;
@@ -41,13 +42,13 @@ internal sealed class CustomGamemode : IVersusGamemode
     // Make Zombie have first pick in Custom
     private static IEnumerator CoWaitSeedChooserVSSwap()
     {
+        // Wait for SeedChooserVSSwap to exist or exit if not in lobby
         while (UnityEngine.Object.FindObjectOfType<SeedChooserVSSwap>() == null)
         {
             if (!ReloadedLobby.AmInLobby())
             {
                 yield break;
             }
-
             yield return null;
         }
 
@@ -57,50 +58,86 @@ internal sealed class CustomGamemode : IVersusGamemode
         ];
 
         IArenaSetupSeedbank.AddInitialSeedsToBanks();
-
         ReloadedClientData.LocalClient?.Ready.Value = true;
 
-        if (ModInfo.DEBUG)
+        if (ModInfo.DEBUG && ReloadedLobby.GetLobbyMemberCount() == 1)
         {
-            if (ReloadedLobby.GetLobbyMemberCount() == 1)
+            if (VersusState.AmPlantSide)
             {
-                // Set up opponent seed bank for debugging
-                var opponentSeedBankInfo = PvZRUtils.GetOpponentSeedBankInfo();
-                if (VersusState.AmPlantSide)
-                {
-                    SeedType[] quickPlayZombies = IArenaSetupSeedbank.GetQuickPlayZombies();
-                    for (int i = opponentSeedBankInfo.mSeedsInBank; i < quickPlayZombies.Length; i++)
-                    {
-                        SeedType seedType = quickPlayZombies[i];
-                        opponentSeedBankInfo.AddSeedFromChooser(seedType);
-                    }
+                SetupDebugPlantSide();
+            }
 
-                    var seedChooserVSSwapDebug = UnityEngine.Object.FindObjectOfType<SeedChooserVSSwap>();
-                    seedChooserVSSwapDebug.playerTurn = 1;
-                    seedChooserVSSwapDebug.GetComponent<VersusChooserSwapBinder>().PlayerTurn = 1;
-                    Instances.GameplayActivity.VersusMode.Phase = VersusPhase.ChoosePlantPacket;
-
-                    yield break;
-                }
-                else if (VersusState.AmZombieSide)
-                {
-                    SeedType[] quickPlayPlants = IArenaSetupSeedbank.GetQuickPlayPlants();
-                    for (int i = opponentSeedBankInfo.mSeedsInBank; i < quickPlayPlants.Length; i++)
-                    {
-                        SeedType seedType = quickPlayPlants[i];
-                        opponentSeedBankInfo.AddSeedFromChooser(seedType);
-                    }
-                }
+            if (VersusState.AmZombieSide)
+            {
+                SetupDebugZombieSide();
+                SetupTurnOrder();
             }
         }
+        else
+        {
+            SetupTurnOrder();
+        }
 
-        // Set fisst turn to Zombie by default
+        HideDisallowedSeeds(chosenSeeds);
+    }
+
+    private static void SetupDebugPlantSide()
+    {
+        var opponentSeedBankInfo = PvZRUtils.GetOpponentSeedBankInfo();
+        SeedType[] quickPlayZombies = IArenaSetupSeedbank.GetQuickPlayZombies();
+
+        for (int i = opponentSeedBankInfo.mSeedsInBank; i < quickPlayZombies.Length; i++)
+        {
+            opponentSeedBankInfo.AddSeedFromChooser(quickPlayZombies[i]);
+        }
+
+        var seedChooserVSSwap = UnityEngine.Object.FindObjectOfType<SeedChooserVSSwap>();
+        seedChooserVSSwap.playerTurn = 1;
+        seedChooserVSSwap.GetComponent<VersusChooserSwapBinder>().PlayerTurn = 1;
+        Instances.GameplayActivity.VersusMode.Phase = VersusPhase.ChoosePlantPacket;
+
+        var versusChooserSwapBinder = seedChooserVSSwap.GetComponent<VersusChooserSwapBinder>();
+        var layer = versusChooserSwapBinder.m_player1Input.Cast<IInputNavigationLayer>();
+        InputNavigationManager.Instance.RemoveLayer(layer);
+    }
+
+    private static void SetupDebugZombieSide()
+    {
+        var opponentSeedBankInfo = PvZRUtils.GetOpponentSeedBankInfo();
+        SeedType[] quickPlayPlants = IArenaSetupSeedbank.GetQuickPlayPlants();
+
+        for (int i = opponentSeedBankInfo.mSeedsInBank; i < quickPlayPlants.Length; i++)
+        {
+            opponentSeedBankInfo.AddSeedFromChooser(quickPlayPlants[i]);
+        }
+    }
+
+    private static void SetupTurnOrder()
+    {
         var seedChooserVSSwap = UnityEngine.Object.FindObjectOfType<SeedChooserVSSwap>();
         seedChooserVSSwap.swapCanvasOrder();
         seedChooserVSSwap.m_vsSeedChooserAnimator.Play(-160334332, 0, 1f);
         seedChooserVSSwap.playerTurn = 1;
-        seedChooserVSSwap.GetComponent<VersusChooserSwapBinder>().PlayerTurn = 1;
 
+        var versusChooserSwapBinder = seedChooserVSSwap.GetComponent<VersusChooserSwapBinder>();
+        versusChooserSwapBinder.PlayerTurn = 1;
+
+        if (VersusState.AmZombieSide)
+        {
+            seedChooserVSSwap.transform.Find("SeedChooser_Player2/IgnoreInput")?.gameObject.SetActive(false);
+            versusChooserSwapBinder.m_player2Input.IgnoreInput1 = false;
+            InputNavigationManager.Instance.SetCurrentLayer(versusChooserSwapBinder.m_player2Input.Cast<IInputNavigationLayer>());
+            versusChooserSwapBinder.m_player1Input.IgnoreInput1 = true;
+        }
+        else if (VersusState.AmPlantSide)
+        {
+            InputNavigationManager.Instance.RemoveLayer(versusChooserSwapBinder.m_player1Input.Cast<IInputNavigationLayer>());
+            versusChooserSwapBinder.m_player2Input.IgnoreInput1 = true;
+        }
+    }
+
+    private static void HideDisallowedSeeds(List<ChosenSeed> chosenSeeds)
+    {
         foreach (var seedPacket in chosenSeeds)
         {
             if (!ICharacterConfig.IsAllowedInArena(seedPacket.mSeedType, VersusState.Arena))
