b8c079297ce9753f0f69af7c30e41e3d3fea0c77
D1GQ <yourboylax@gmail.com>
2026-05-12 23:10:28 -0500

    Fix Bungee render order

--- src/Modules/Reloaded/Versus/Configs/Zombie/BungeeZombieConfig.cs
+++ src/Modules/Reloaded/Versus/Configs/Zombie/BungeeZombieConfig.cs
@@ -6,26 +6,21 @@ using ReplantedOnline.Interfaces.Versus;
 
 namespace ReplantedOnline.Modules.Reloaded.Versus.Configs.Zombie;
 
-[RegisterZombieConfig(ZombieType.Digger)]
-internal sealed class DiggerZombieConfig : IZombieConfig
+[RegisterZombieConfig(ZombieType.Bungee)]
+internal sealed class BungeeZombieConfig : IZombieConfig
 {
     /// <inheritdoc/>
     public void SetArenaDefinition(ZombieDefinition zombieDefinition, ArenaTypes arena) { }
 
     /// <inheritdoc/>
-    public bool IsAllowedInArena(ArenaTypes arena)
-    {
-        if (arena is ArenaTypes.Roof or ArenaTypes.RoofNight)
-        {
-            return false;
-        }
-
-        return true;
-    }
+    public bool IsAllowedInArena(ArenaTypes arena) => true;
 
     /// <inheritdoc/>
     public bool CanBePlacedAt(ArenaTypes arena, int gridX, int gridY) => true;
 
     /// <inheritdoc/>
-    public void OnPlanted(Il2CppReloaded.Gameplay.Zombie zombie, int gridX, int gridY) { }
+    public void OnPlanted(Il2CppReloaded.Gameplay.Zombie zombie, int gridX, int gridY)
+    {
+        zombie.mRenderOrder -= 400;
+    }
 }
