46e3e1b0b164ab9cc97af6ff1d701a465e560696
D1GQ <yourboylax@gmail.com>
2026-05-25 17:36:55 -0500

    Add fog logic

--- src/Modules/Reloaded/Versus/Arenas/PoolNightArena.cs
+++ src/Modules/Reloaded/Versus/Arenas/PoolNightArena.cs
@@ -3,6 +3,7 @@ using Il2CppReloaded.Gameplay;
 using Il2CppReloaded.Services;
 using ReplantedOnline.Attributes.Register;
 using ReplantedOnline.Enums.Versus;
+using ReplantedOnline.Modules.Modded.Instance;
 using ReplantedOnline.Patches.Reloaded.Gameplay.Versus.Arenas;
 using UnityEngine;
 
@@ -37,14 +38,12 @@ internal sealed class PoolNightArena : PoolArena
 
         if (VersusState.AmPlantSide)
         {
-            versusMode.m_app.BackgroundController.FogController.SetTargetOriginal(2, TodCurves.EaseIn, 0, 5);
+            versusMode.m_app.BackgroundController.FogController.SetTargetOriginal(0, TodCurves.Constant, 0, 0);
         }
         else if (VersusState.AmZombieSide)
         {
-            var pos = versusMode.m_app.BackgroundController.FogController.transform.localPosition;
-            versusMode.m_app.BackgroundController.FogController.transform.localPosition = new Vector3(0f, pos.y, pos.z);
             versusMode.m_app.BackgroundController.FogController.transform.localScale = new Vector3(-1f, 1f, 1f);
-            versusMode.m_app.BackgroundController.FogController.SetTargetOriginal(7, TodCurves.EaseIn, 0, 5);
+            versusMode.m_app.BackgroundController.FogController.SetTargetOriginal(0, TodCurves.Constant, 0, 0);
         }
     }
 
@@ -63,13 +62,44 @@ internal sealed class PoolNightArena : PoolArena
         }
     }
 
+    private float VersusTime => Instances.GameplayActivity.VersusMode.m_versusTime;
+    private static readonly float VersusTimeEnd = VersusMode.k_suddenDeathStartTime - 3f;
+
+    private readonly float OffScreenPlantFog = 4200f;
+    private readonly float StartPlantFog = 2100f;
+    private readonly float EndPlantFog = 3100f;
+
     private void UpdateFogPlants()
     {
-
+        if (VersusTime < 3f)
+        {
+            SetFogPos(Mathf.Lerp(OffScreenPlantFog, StartPlantFog, VersusTime / 3f));
+        }
+        else
+        {
+            SetFogPos(Mathf.Lerp(StartPlantFog, EndPlantFog, VersusTime / VersusTimeEnd));
+        }
     }
+
+    private readonly float OffScreenZombieFog = 0f;
+    private readonly float StartZombieFog = 2393f;
+    private readonly float EndZombieFog = 1041f;
 
     private void UpdateFogZombies()
     {
+        if (VersusTime < 3f)
+        {
+            SetFogPos(Mathf.Lerp(OffScreenZombieFog, StartZombieFog, VersusTime / 3f));
+        }
+        else
+        {
+            SetFogPos(Mathf.Lerp(StartZombieFog, EndZombieFog, VersusTime / VersusTimeEnd));
+        }
+    }
 
+    private static void SetFogPos(float pos)
+    {
+        var fog = Instances.GameplayActivity.BackgroundController.FogController;
+        fog.transform.localPosition = new Vector3(pos, -1150f, 0f);
     }
 }
