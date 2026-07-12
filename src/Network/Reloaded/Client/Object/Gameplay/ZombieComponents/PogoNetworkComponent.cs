6b8d2d889dbfb4da55dc83661611200ea2510e5b
D1GQ <yourboylax@gmail.com>
2026-07-12 14:43:54 -0500

    Rework syncing for pogo zombie

--- src/Network/Reloaded/Client/Object/Gameplay/ZombieComponents/PogoNetworkComponent.cs
+++ src/Network/Reloaded/Client/Object/Gameplay/ZombieComponents/PogoNetworkComponent.cs
@@ -1,6 +1,8 @@
 ﻿using Il2CppReloaded.Gameplay;
+using ReplantedOnline.Attributes.Network;
 using ReplantedOnline.Attributes.Register;
 using ReplantedOnline.Network.Reloaded.Client.Object.Gameplay.Components;
+using ReplantedOnline.Patches.Reloaded.Gameplay.Versus.Zombies;
 
 namespace ReplantedOnline.Network.Reloaded.Client.Object.Gameplay.ZombieComponents;
 
@@ -8,17 +10,43 @@ namespace ReplantedOnline.Network.Reloaded.Client.Object.Gameplay.ZombieComponen
 [RegisterNetworkComponent(ZombieType.Pogo)]
 internal sealed class PogoNetworkComponent : ZombieNetworkComponent
 {
+    private enum PogoRpcs : byte
+    {
+        JumpOver,
+        PogoBreak
+    }
+
+    private bool _inJump;
+
+    internal sealed override void Update()
+    {
+        if (Net.Zombie == null) return;
+
+        if (Net.Zombie.mZombiePhase == ZombiePhase.RisingFromGrave) return;
+
+        if (Net.AmOwner)
+        {
+            if (Net.Zombie.mZombiePhase == ZombiePhase.PogoForwardBounce2 && !_inJump)
+            {
+                _inJump = true;
+                SendJumpOverRpc();
+            }
+        }
+
+        if (Net.Zombie.mZombiePhase != ZombiePhase.PogoForwardBounce2)
+        {
+            _inJump = false;
+        }
+    }
+
     internal override void UpdatePosition(float distance, bool useNonNetworkLogic = false)
     {
         if (Net.Zombie == null)
-        {
-            base.UpdatePosition(distance, useNonNetworkLogic);
             return;
-        }
 
         if (!Net.AmOwner)
         {
-            if (Net.Zombie.mZombiePhase is not (ZombiePhase.ZombieNormal or ZombiePhase.PogoHighBounce1))
+            if (_inJump)
             {
                 base.UpdatePosition(distance, true);
                 return;
@@ -26,5 +54,41 @@ internal sealed class PogoNetworkComponent : ZombieNetworkComponent
         }
 
         base.UpdatePosition(distance, useNonNetworkLogic);
+    }
+
+    private void SendJumpOverRpc()
+    {
+        if (Net.Zombie == null)
+            return;
+
+        SendNetworkComponentRpc(PogoRpcs.JumpOver, Net.Zombie.mPhaseCounter);
+    }
+
+    [RpcHandler(PogoRpcs.JumpOver)]
+    private void HandleJumpOverRpc(int PhaseCounter)
+    {
+        if (Net.Zombie == null)
+            return;
+
+        _inJump = true;
+        Net.Zombie.mPhaseCounter = PhaseCounter;
+        Net.Zombie.mZombiePhase = ZombiePhase.PogoForwardBounce2;
+    }
+
+    internal void SendPogoBreakRpc(DamageFlags damageFlags)
+    {
+        if (Net.Zombie == null)
+            return;
+
+        SendNetworkComponentRpc(PogoRpcs.PogoBreak, damageFlags);
+    }
+
+    [RpcHandler(PogoRpcs.PogoBreak)]
+    private void HandlePogoBreakRpc(DamageFlags damageFlags)
+    {
+        if (Net.Zombie == null)
+            return;
+
+        Net.Zombie.PogoBreakOriginal(damageFlags);
     }
 }
---
+++ src/Patches/Reloaded/Gameplay/Versus/Zombies/PogoZombiePatch.cs
@@ -0,0 +1,58 @@
+﻿using HarmonyLib;
+using Il2CppReloaded.Gameplay;
+using ReplantedOnline.Modules.Reloaded.Versus;
+using ReplantedOnline.Network.Reloaded.Client;
+using ReplantedOnline.Network.Reloaded.Client.Object.Gameplay.ZombieComponents;
+using ReplantedOnline.Utilities.Modded;
+using Zombie = Il2CppReloaded.Gameplay.Zombie;
+
+namespace ReplantedOnline.Patches.Reloaded.Gameplay.Versus.Zombies;
+
+[HarmonyPatch]
+internal static class PogoZombiePatch
+{
+    [HarmonyPatch(typeof(Zombie), nameof(Zombie.FindPlantTarget))]
+    [HarmonyPostfix]
+    private static void Zombie_FindPlantTarget_Postfix(Zombie __instance, ZombieAttackType theAttackType, ref Plant? __result)
+    {
+        if (__instance.mZombieType != ZombieType.Pogo) return;
+        if (theAttackType != ZombieAttackType.Vault) return;
+
+        if (ReloadedLobby.AmInLobby())
+        {
+            if (!VersusState.AmPlantSide)
+            {
+                __result = null;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(Zombie), nameof(Zombie.PogoBreak))]
+    [HarmonyPrefix]
+    private static bool Zombie_PogoBreak_Prefix(Zombie __instance, DamageFlags theDamageFlags)
+    {
+        if (ReloadedLobby.AmInLobby())
+        {
+            if (!VersusState.AmPlantSide)
+                return false;
+
+            var zombieNetworked = __instance.GetNetworked();
+            if (zombieNetworked != null)
+            {
+                if (zombieNetworked.TryGetNetworkComponent<PogoNetworkComponent>(out var comp))
+                {
+                    comp.SendPogoBreakRpc(theDamageFlags);
+                }
+            }
+        }
+
+        return true;
+    }
+
+    [HarmonyReversePatch]
+    [HarmonyPatch(typeof(Zombie), nameof(Zombie.PogoBreak))]
+    internal static void PogoBreakOriginal(this Zombie __instance, DamageFlags theDamageFlags)
+    {
+        throw new NotImplementedException("Reverse Patch Stub");
+    }
+}
