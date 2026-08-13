5671d64bc0c1c85f806a10a89eadd783b4615d60
D1GQ <yourboylax@gmail.com>
2026-08-13 16:13:49 -0500

    Run on main thread

--- src/Network/Discord/DiscordManager.cs
+++ src/Network/Discord/DiscordManager.cs
@@ -291,13 +291,13 @@ internal static class DiscordManager
     /// <param name="args">The join message containing the lobby secret.</param>
     private static void OnJoin(object sender, JoinMessage args)
     {
-        if (ReloadedLobby.AmInLobby())
-        {
-            return;
-        }
-
         MainThreadDispatcher.Execute(() =>
         {
+            if (ReloadedLobby.AmInLobby())
+            {
+                return;
+            }
+
             DiscordLobbySecret secret = DiscordLobbySecret.Deserialize(args.Secret);
 
             if (secret.FormatError)
